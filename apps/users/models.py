55567a7343062ac0f8da4b79716f8d183b7be732
Cory Zue <czue@dimagi.com>
2020-04-30 09:21:25 +0200

    combine user models

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,7 +1,35 @@
-from pegasus.apps.users.models import PegasusUser
+import hashlib
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
-class CustomUser(PegasusUser):
+class CustomUser(AbstractUser):
     """
     Add additional fields to the user model here.
     """
+    """
+    Abstract base class for users, with a small amount of added functionality
+    """
+    avatar = models.FileField(upload_to='profile-pictures/', null=True, blank=True)
+
+    def __str__(self):
+        return self.email
+
+    def get_display_name(self):
+        if self.get_full_name().strip():
+            return self.get_full_name()
+        return self.email
+
+    @property
+    def avatar_url(self):
+        default_avatar_image = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/User_font_awesome.svg/512px-User_font_awesome.svg.png'
+        if self.avatar:
+            return self.avatar.url
+        else:
+            return 'https://www.gravatar.com/avatar/{}?s=128&d={}'.format(self.gravatar_id, default_avatar_image)
+
+    @property
+    def gravatar_id(self):
+        # https://en.gravatar.com/site/implement/hash/
+        return hashlib.md5(self.email.lower().strip().encode('utf-8')).hexdigest()
