0d99f70d974216691a965804a52bda19c1fcc3c7
Cory Zue <czue@dimagi.com>
2020-04-30 09:57:12 +0200

    add string functions and display names

--- apps/exports/models.py
+++ apps/exports/models.py
@@ -12,14 +12,21 @@ class BaseModel(models.Model):
     class Meta:
         abstract = True
 
+
 class CommCareServer(BaseModel):
     name = models.CharField(max_length=100, default='CommCare HQ')
     url = models.CharField(max_length=100, default=settings.COMMCARE_DEFAULT_SERVER)
+
+    def __str__(self):
+        return f'{self.name} ({self.url})'
 
 
 class CommCareProject(BaseModel):
     server = models.ForeignKey(CommCareServer, on_delete=models.CASCADE)
     domain = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.domain} ({self.server.name})'
 
 
 class CommCareAccount(BaseModel):
@@ -27,13 +34,24 @@ class CommCareAccount(BaseModel):
     username = models.CharField(max_length=100)
     api_key = models.CharField(max_length=40)
 
+    def __str__(self):
+        return f'{self.username} ({self.server.name})'
+
 
 class ExportDatabase(BaseModel):
+    name = models.CharField(max_length=100)
     connection_string = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class ExportConfig(BaseModel):
+    name = models.CharField(max_length=100)
     project = models.ForeignKey(CommCareProject, on_delete=models.CASCADE)
     account = models.ForeignKey(CommCareAccount, on_delete=models.CASCADE)
     database = models.ForeignKey(ExportDatabase, on_delete=models.CASCADE)
     config_file = models.FileField(upload_to='export-configs/', null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name} - {self.project}'
