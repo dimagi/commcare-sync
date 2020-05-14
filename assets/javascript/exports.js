df532e92acf0dadc4e7df284c81d9c9635991523
Cory Zue <czue@dimagi.com>
2020-05-14 16:29:59 +0200

    externalize api request and progress update

--- assets/javascript/exports.js
+++ assets/javascript/exports.js
@@ -1,4 +1,4 @@
-
+import Cookies from 'js-cookie';
 
 const addLogStubToTable = function () {
   // ht: https://stackoverflow.com/a/18333693/8207
@@ -38,7 +38,68 @@ const setupLogTriggers = function () {
 
 };
 
+
+const initializeExportRunButton = function (apiUrl, progressUrl) {
+  const runExportButton = document.getElementById('run-export-button');
+  runExportButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    runExportButton.classList.add('is-loading', 'is-disabled');
+    const progressWrapper = document.getElementById('export-status-progress');
+    progressWrapper.style.display = 'inherit';
+    const progressBar = document.getElementById('progress-bar');
+    const progressMessage = document.getElementById('progress-bar-message');
+    fetch(apiUrl, {
+      method: 'post',
+      credentials: 'same-origin',
+      headers: {
+        'X-CSRFToken': Cookies.get('csrftoken'),
+      }
+    }).then(
+      function (response) {
+        return response.text();
+      }).then(function (text) {
+
+      const taskUrl = progressUrl.replace('task-id-stub', text);
+      const recordRow = Exports.addLogStubToTable();
+      if (typeof CeleryProgressBar === 'undefined') {
+        const errorMessage = 'CeleryProgressBar class missing. Did you forget to import celery_progress.js?';
+        console.error(errorMessage);
+        progressBar.classList.add('is-danger');
+        progressBar.setAttribute('value', '100');
+        progressMessage.innerText = errorMessage;
+        return;
+      }
+      CeleryProgressBar.initProgressBar(taskUrl, {
+        onProgress: function () {
+          progressMessage.innerHTML = 'Running Export...';
+        },
+        onSuccess: function () {
+          progressBar.setAttribute('value', '100');
+          progressMessage.innerHTML = 'Complete!';
+        },
+        onResult: function (resultElement, result) {
+          recordRow.cells[1].innerText = result.duration;
+          recordRow.cells[2].innerText = result.status;
+          if (result.status === "completed") {
+            progressBar.classList.add('is-success');
+          } else if (result.status === "failed") {
+            progressBar.classList.add('is-danger');
+            progressBar.setAttribute('value', '100');
+            progressMessage.innerText = 'Failed!';
+          }
+          recordRow.cells[3].innerHTML = '<a onclick=location.reload()>Refresh for log</a>';
+          runExportButton.classList.remove('is-loading', 'is-disabled');
+        }
+      });
+    })
+      .catch(function (error) {
+        console.error('Request failed', error);
+      });
+  }, false);
+};
+
 export const Exports = {
   setupLogTriggers: setupLogTriggers,
   addLogStubToTable: addLogStubToTable,
+  initializeExportRunButton: initializeExportRunButton,
 };
