58f84caa703f67ffac86370cf2e3b036399ed30a
Cory Zue <czue@dimagi.com>
2020-06-22 15:14:19 +0200

    condense run status details and use natural times

---
+++ apps/exports/templatetags/exports_tags.py
@@ -0,0 +1,21 @@
+from django import template
+from django.utils.safestring import mark_safe
+
+from apps.exports.models import ExportRunBase
+
+register = template.Library()
+
+@register.filter()
+def to_status_icon(export_status):
+    text_modifiers = {
+        ExportRunBase.COMPLETED: 'has-text-success',
+        ExportRunBase.FAILED: 'has-text-danger',
+        ExportRunBase.STARTED: 'has-text-primary',
+    }
+    icons = {
+        ExportRunBase.COMPLETED: 'fa-check-circle',
+        ExportRunBase.FAILED: 'fa-exclamation-circle',
+        ExportRunBase.STARTED: 'fa-ellipsis-h',
+    }
+    return mark_safe(f'<span class="icon {text_modifiers.get(export_status)}"><i title="{export_status}" class="fa {icons.get(export_status)}"></i></span>')
+
--- commcare_sync/settings.py
+++ commcare_sync/settings.py
@@ -35,6 +35,7 @@ DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.humanize',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
