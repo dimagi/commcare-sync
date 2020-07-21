2da877e07edc3274fbb80aea745d0b033df61f9e
Cory Zue <czue@dimagi.com>
2020-07-21 10:59:32 +0200

    fix migration file to rename index
    https://code.djangoproject.com/ticket/23577#comment:17

--- apps/exports/migrations/0009_auto_20200721_1039.py
+++ apps/exports/migrations/0009_auto_20200721_1039.py
@@ -15,4 +15,8 @@ class Migration(migrations.Migration):
             old_name='MultiProjectExportRun',
             new_name='MultiProjectPartialExportRun',
         ),
+        # rename the index to workaround this bug https://code.djangoproject.com/ticket/23577#comment:17
+        migrations.RunSQL(
+            'ALTER INDEX exports_multiprojectexportrun_export_config_id_bcc53671 rename TO exports_multiprojectpartialexportrun_export_config_id_bcc53671myapp_mymodel_myfield_othermodel_id_0f4cfc54',
+        ),
     ]
