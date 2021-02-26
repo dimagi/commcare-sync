b70cd2e64d19a22fb9038947e95e8e4789ad77a1
Farid Rener <proteusvacuum@gmail.com>
2021-02-26 16:50:39 -0500

    Add BaseExportTestCase

--- apps/exports/tests/test_runner.py
+++ apps/exports/tests/test_runner.py
@@ -1,13 +1,5 @@
-from apps.commcare.models import (
-    CommCareAccount,
-    CommCareProject,
-    CommCareServer,
-)
-from apps.exports.models import ExportConfig, ExportDatabase
 from apps.exports.runner import _compile_export_command
-from apps.exports.tests.test_utils import BaseSimpleExportTestCase
-from django.core.files.uploadedfile import TemporaryUploadedFile
-from django.test import TestCase
+from apps.exports.tests.test_utils import BaseSimpleExportTestCase, BaseExportTestCase
 
 
 class TestRunnerArguments(BaseSimpleExportTestCase):
@@ -18,27 +10,9 @@ class TestRunnerArguments(BaseSimpleExportTestCase):
         self.assertIn(self.server.url, command)
 
 
-class RunnerArgumentsDbTest(TestCase):
+class RunnerArgumentsDbTest(BaseExportTestCase):
 
     def test_default_server_url(self):
-        server = CommCareServer.objects.get()
-        project = CommCareProject(server=server, domain="foo")
-        account = CommCareAccount(server=server, username="foo", api_key="P@ssWord")
-        database = ExportDatabase(
-            name="Test DB", connection_string="postgresql://foo:bar@123.4.0.0/test"
-        )
-        config_file = TemporaryUploadedFile(
-            name="config_file", content_type="application/xml", size=100, charset="utf-8"
-        )
-        export_config = ExportConfig(
-            name="Test Config",
-            project=project,
-            account=account,
-            database=database,
-            config_file=config_file,
-            extra_args="",
-        )
-
-        command = _compile_export_command(export_config, project, force=False)
+        command = _compile_export_command(self.export_config, self.project, force=False)
 
         self.assertIn('https://www.commcarehq.org', command)
--- apps/exports/tests/test_utils.py
+++ apps/exports/tests/test_utils.py
@@ -1,13 +1,15 @@
-from django.test import SimpleTestCase
-from django.core.files.uploadedfile import TemporaryUploadedFile
-
-from apps.commcare.models import CommCareServer, CommCareProject, CommCareAccount
-
+from apps.commcare.models import (
+    CommCareAccount,
+    CommCareProject,
+    CommCareServer,
+)
 from apps.exports.models import ExportConfig, ExportDatabase
+from django.contrib.auth import get_user_model
+from django.core.files.uploadedfile import TemporaryUploadedFile
+from django.test import SimpleTestCase, TestCase
 
 
 class BaseSimpleExportTestCase(SimpleTestCase):
-
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -28,3 +30,42 @@ class BaseSimpleExportTestCase(SimpleTestCase):
             config_file=cls.config_file,
             extra_args="",
         )
+
+
+class BaseExportTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = get_user_model().objects.create(username="test_user", email="P@ssWord")
+        cls.server = CommCareServer.objects.get()
+        cls.project = CommCareProject.objects.create(server=cls.server, domain="foo")
+        cls.account = CommCareAccount.objects.create(
+            server=cls.server, username="foo", api_key="P@ssWord", owner=cls.user
+        )
+        cls.database = ExportDatabase.objects.create(
+            name="Test DB",
+            connection_string="postgresql://foo:bar@123.4.0.0/test",
+            owner=cls.user,
+        )
+        config_file = TemporaryUploadedFile(
+            name="config_file", content_type="application/xml", size=100, charset="utf-8"
+        )
+        cls.export_config = ExportConfig.objects.create(
+            name="Test Config",
+            project=cls.project,
+            account=cls.account,
+            database=cls.database,
+            config_file=config_file,
+            extra_args="",
+            created_by=cls.user,
+        )
+        config_file.close()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.export_config.delete()
+        cls.database.delete()
+        cls.account.delete()
+        cls.project.delete()
+        cls.user.delete()
+        super().tearDownClass()
