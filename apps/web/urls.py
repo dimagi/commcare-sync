fd6a2aba71d5b87647112ab67602549b3ec36a18
Cory Zue <czue@dimagi.com>
2021-06-04 12:18:57 +0200

    fix infinite redirect when creating a DB as a non-admin

--- apps/web/urls.py
+++ apps/web/urls.py
@@ -6,5 +6,6 @@ from . import views
 app_name = 'web'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('admin-required', views.admin_required, name='admin_required'),
     path('terms', TemplateView.as_view(template_name="web/terms.html"), name='terms'),
 ]
