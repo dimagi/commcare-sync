ed1ef697e617534ce0a5f3869191a4b3a6d70c27
Norman Hooper <nhooper@dimagi.com>
2022-04-13 01:37:39 +0100

    Add more modules

--- apps/web/context_processors.py
+++ apps/web/context_processors.py
@@ -1,9 +1,11 @@
 from copy import copy
+from typing import Any, Dict
 from django.conf import settings
+from django.http import HttpRequest
 from .meta import absolute_url
 
 
-def project_meta(request):
+def project_meta(request: HttpRequest) -> Dict[str, Any]:
     # modify these values as needed and add whatever else you want globally available here
     project_data = copy(settings.PROJECT_METADATA)
     project_data['TITLE'] = '{} | {}'.format(project_data['NAME'], project_data['DESCRIPTION'])
@@ -16,7 +18,7 @@ def project_meta(request):
     }
 
 
-def google_analytics_id(request):
+def google_analytics_id(request: HttpRequest) -> Dict[str, Any]:
     """
     Adds google analytics id to all requests
     """
--- apps/web/meta.py
+++ apps/web/meta.py
@@ -1,5 +1,5 @@
 from django.contrib.sites.models import Site
 
 
-def absolute_url(relative_url):
+def absolute_url(relative_url: str) -> str:
     return 'https://{}{}'.format(Site.objects.get_current().domain, relative_url)
