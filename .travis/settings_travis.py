7560598c78c9ffadd1a019584c0e20601f261ed0
Norman Hooper <nhooper@dimagi.com>
2026-01-02 20:05:13 +0000

    Values for Travis

--- .travis/settings_travis.py
+++ .travis/settings_travis.py
@@ -1,5 +1,7 @@
 from .settings import *  # noqa
 
+SECRET_KEY = 'it(5855w8z-6)9%&71n=u!upv1q7sl^xh!iisymue9v5xk36an'
+FERNET_KEYS = ['sGylGjTyNm64l4-F448DiMjS6cE1qZ9b5UZWPxTu2po=']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
