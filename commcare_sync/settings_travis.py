710b6badee7a20a61541458bebbe62f901542537
Norman Hooper <nhooper@dimagi.com>
2026-01-15 07:53:34 +0000

    settings_travis.py works like settings_local.py

--- commcare_sync/settings_travis.py
+++ commcare_sync/settings_travis.py
@@ -1,4 +1,4 @@
-from .settings import *  # noqa
+# Settings for Travis CI
 
 SECRET_KEY = 'it(5855w8z-6)9%&71n=u!upv1q7sl^xh!iisymue9v5xk36an'
 FERNET_KEYS = ['sGylGjTyNm64l4-F448DiMjS6cE1qZ9b5UZWPxTu2po=']
