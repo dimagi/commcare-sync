87769514d6e4f89a85f5ecd8ff9b3d0eb9eda754
Norman Hooper <nhooper@dimagi.com>
2026-03-12 22:43:46 +0000

    feat: add database views and URLs to apps.db

---
+++ apps/db/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+
+app_name = 'db'
+urlpatterns = [
+    path(r'', views.databases, name='databases'),
+    path(r'create/', views.create_database, name='create_database'),
+    path(r'<int:database_id>/edit/', views.edit_database, name='edit_database'),
+    path(r'<int:database_id>/delete/', views.delete_database, name='delete_database'),
+]
