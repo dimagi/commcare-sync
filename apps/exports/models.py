55101bfa423c44c1622ab2de4b504cea819e6e1e
Norman Hooper <nhooper@dimagi.com>
2026-03-20 18:03:35 +0000

    fixup! feat: add ScheduleMixin to ExportConfigBase (schema migration)

--- apps/exports/models.py
+++ apps/exports/models.py
@@ -30,19 +30,6 @@ class ExportConfigBase(ScheduleMixin, BaseModel):
         abstract = True
 
     @property
-    def schedule_display(self):
-        if self.is_paused:
-            return ''
-        minutes = self.time_between_runs
-        if minutes % (60 * 24) == 0:
-            days = minutes // (60 * 24)
-            return f'Every {days} day{"s" if days != 1 else ""}'
-        elif minutes % 60 == 0:
-            hours = minutes // 60
-            return f'Every {hours} hour{"s" if hours != 1 else ""}'
-        return f'Every {minutes} minutes'
-
-    @property
     def last_run(self):
         all_runs = getattr(self, '_all_runs', None)
         if all_runs is not None:
