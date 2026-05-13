437b5b5f3234666e75748381f1df3e49190c6a4e
Norman Hooper <nhooper@dimagi.com>
2026-05-13 00:35:18 -0400

    feat(refreshes): add stat cards to refreshes list page

--- apps/refreshes/tests/test_views.py
+++ apps/refreshes/tests/test_views.py
@@ -35,6 +35,14 @@ class TestRefreshConfigsListView:
         assert response.status_code == 302
         assert '/accounts/login/' in response.url
 
+    @use(authed_client, 'db')
+    def test_stats_in_context(self):
+        url = reverse('refreshes:refresh_configs')
+        response = authed_client().get(url)
+        assert 'export_stats' in response.context
+        assert 'refresh_stats' in response.context
+        assert 'forwarding_stats' in response.context
+
     @use(authed_client, _refresh_config)
     def test_list_configs(self):
         config = _refresh_config()
