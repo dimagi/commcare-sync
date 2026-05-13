d3f8ae159577d49a6b6451fc36c9912879dfea25
Norman Hooper <nhooper@dimagi.com>
2026-05-13 00:35:15 -0400

    refactor(web): rename admin_required view to admin_access_denied

--- apps/web/views.py
+++ apps/web/views.py
@@ -184,7 +184,7 @@ def _get_forwarding_statistics(since_datetime):
 
 
 @login_required
-def admin_required(request):
+def admin_access_denied(request):
     if request.user.is_superuser:
         return HttpResponseRedirect(request.GET.get('next', reverse('web:home')))
     else:
