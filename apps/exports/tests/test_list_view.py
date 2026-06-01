07567391edebc799e0b556d55d8dcb6f5e8751b9
Norman Hooper <nhooper@dimagi.com>
2026-06-01 17:37:58 -0400

    feat(exports): add edit_url and last_run_log_url to ExportConfigBase

--- apps/exports/tests/test_list_view.py
+++ apps/exports/tests/test_list_view.py
@@ -1,8 +1,30 @@
 from django.urls import reverse
 from unmagic import use
 
-from apps.exports.tests.fixtures import export_config
+from apps.exports.tests.fixtures import export_config, multi_export_config
 from tests.fixtures import authed_client
+
+
+class TestExportConfigBaseProperties:
+    @use(export_config)
+    def test_export_config_edit_url(self):
+        config = export_config()
+        expected = reverse('exports:edit_export_config', args=[config.id])
+        assert config.edit_url == expected
+
+    @use(multi_export_config)
+    def test_multi_export_config_edit_url(self):
+        config = multi_export_config()
+        expected = reverse('exports:edit_multi_export_config', args=[config.id])
+        assert config.edit_url == expected
+
+    @use(export_config)
+    def test_last_run_log_url_none_when_no_run(self):
+        assert export_config().last_run_log_url is None
+
+    @use(multi_export_config)
+    def test_last_run_log_url_none_when_no_run_multi(self):
+        assert multi_export_config().last_run_log_url is None
 
 
 class TestExportsHomeView:
