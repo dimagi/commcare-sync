c2a13548a83e4ff2d105aceafc60466ee0fdd0a3
Norman Hooper <nhooper@dimagi.com>
2026-06-19 19:24:31 +0100

    Add unit tests for paginate() and render_config_table()

--- commcare_sync/tests/test_views.py
+++ commcare_sync/tests/test_views.py
@@ -1,11 +1,22 @@
+from django.core.paginator import Paginator
 from django.test import RequestFactory
 
 from commcare_sync.views import (
+    compute_configs_etag,
     get_config_page_size,
     get_page_from_request,
     get_run_statuses_from_request,
+    paginate,
+    render_config_table,
 )
 from commcare_sync.consts import VALID_CONFIG_PAGE_SIZES
+
+
+class _FakeConfig:
+    """Stand-in for a config object: has an id but no prefetched ``_all_runs``."""
+
+    def __init__(self, id):
+        self.id = id
 
 
 class TestGetConfigPageSize:
@@ -79,3 +90,33 @@ class TestGetRunStatusesFromRequest:
             'status_filter=completed',
         )))
         assert set(get_run_statuses_from_request(request)) == {'completed'}
+
+
+class TestPaginate:
+    def test_returns_requested_page(self):
+        page = paginate(list(range(25)), 10, 2)
+        assert page.number == 2
+        assert list(page.object_list) == list(range(10, 20))
+
+    def test_out_of_range_clamps_to_last_page(self):
+        page = paginate(list(range(25)), 10, 999)
+        assert page.number == 3
+        assert list(page.object_list) == [20, 21, 22, 23, 24]
+
+    def test_empty_object_list_returns_page_1(self):
+        page = paginate([], 10, 1)
+        assert page.number == 1
+        assert list(page.object_list) == []
+
+
+class TestRenderConfigTable:
+    def _page(self):
+        return Paginator([_FakeConfig(1)], 10).page(1)
+
+    def test_matching_etag_returns_empty_no_swap_response(self):
+        page = self._page()
+        etag = compute_configs_etag(page.object_list)
+        request = RequestFactory().get('/', {'etag': etag})
+        response = render_config_table(request, page, 10, 'tmpl.html', '/url')
+        assert response['HX-Reswap'] == 'none'
+        assert response.content == b''
